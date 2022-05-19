5a218ac171407167fcd18c5a6eff8d95aad25845
dtechdude <davidayomideadedeji@gmail.com>
2022-05-19 12:35:41 +0100

    fixed slider bug on mobile

--- _site/js/slider.js
+++ _site/js/slider.js
@@ -5,21 +5,26 @@ const slider = document.querySelector(".slider-container");
 // const filter = document.querySelector(".engage_filter");
 const btnClose = document.querySelector(".close__btn");
 
+// what caused the bug, having display block and style visibility together and then setting the slider to display none
+// btnSlider.addEventListener("click", function (e) {
+//   slider.style.visibility = "visible";
+//   slider.style.display = "block";
+// });
+// btnClose.addEventListener("click", function (e) {
+//   slider.style.visibility = "hidden";
+//   slider.style.display = "none";
+
+// });
+
 btnSlider.addEventListener("click", function (e) {
   slider.style.visibility = "visible";
   slider.classList.add("slide-in-top");
-
-  slider.style.display = "block";
-
   btnClose.style.display = "block";
-
   e.preventDefault();
 });
 btnClose.addEventListener("click", function (e) {
   slider.style.visibility = "hidden";
-  slider.style.display = "none";
   slider.classList.remove("slide-in-top");
-
   btnClose.style.display = "none";
   e.preventDefault();
 });
