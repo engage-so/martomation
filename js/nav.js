d8777b7c4f613f79c29338c0f5a4c6eafdb0e86b
dtechdude <davidayomideadedeji@gmail.com>
2022-05-19 05:15:15 +0100

    update on meta tags and other utilities

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 // selection for left column - the nav bar
 const sidebarMobile = document.querySelector(".sidebar");
+const glossaryItems = document.querySelector(".glossary__item-container");
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("toggle");
@@ -8,4 +9,7 @@ hamburgerMenu.addEventListener("click", () => {
   sidebarMobile.classList.toggle("dn");
   sidebarMobile.classList.toggle("dn-m");
   sidebarMobile.classList.toggle("db-l");
+  glossaryItems.classList.toggle("dn");
+  glossaryItems.classList.toggle("db-m");
+  glossaryItems.classList.toggle("db-l");
 });
